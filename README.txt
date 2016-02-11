94bdcddde00b8c7b4aba04971838474b66500c63
Antonio <antonio.alves.correia@gmail.com>
2016-02-11 00:39:00 -0300

    Atualizações utilizando textblob

--- README.txt
+++ README.txt
@@ -1,5 +1,7 @@
-Ferramenta de tradução desenvolvida em Python utilizando a goslate.
+Translate tool developed in Python.
 
-A varre arquivos .vtt e traduz as legendas para a linguagem passada como parâmetro.
+sub.tor translates .vtt files in current directory to language passed as a parameter and saves them as .srt files.
 
+
+More about .vtt:
 http://html5doctor.com/video-subtitling-and-webvtt/
--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-import goslate
+#
+# Created by Antonio Correia Feb, 2016
+#
+
+import os
+import glob
+from textblob import TextBlob
 
 translateTo = "pt-br"
 
-os.chdir(dir_formatado)
-for file in glob.glob("*.c"):
+def translate(text):
+    blob = TextBlob(text)
+    return blob.translate(to="pt-br")
+   
+os.chdir(os.getcwd())
 
-vtt_file = open("subtitles.vtt","r")
-vtt_text = vtt_file.read()
-vtt_file.close()
+for file in glob.glob("*.vtt"):
+    print("Traduzindo "+file)
+    
+    sub_file = open(file,"r")
+    sub_text = sub_file.readlines()
+    trans_text = ""
+    
+    for line in sub_text:
+        
+        if not(line[:1].isdigit() or line.strip() == ""):
+            line = str(translate(line))
+
+        if line.strip() == "":
+            line = "\n"
+        
+        trans_text = trans_text + line
+
+    trans_file = open(file[:len(file)-1]+translateTo+".srt","w")
+    trans_file.write(trans_text)
+    trans_file.close()
+    print(file+" traduzido")
+    
+    sub_file.close()
 
 
-gs = goslate.Goslate()
-gs.translate(stringOriginalLanguage,translateTo)
